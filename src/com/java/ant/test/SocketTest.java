a05371943eae7ebcdd23682a1ba8e53e431f3f96
nsit_karan <nsit_karan@yahoo.com>
2016-06-18 08:45:43 -0700

    added initial ivy file + changes to build.xml to use ivy

--- src/com/java/ant/test/SocketTest.java
+++ src/com/java/ant/test/SocketTest.java
@@ -3,13 +3,18 @@ package com.java.ant.test;
 import com.java.ant.networking.SocketConnect;
 
 public class SocketTest {
+	
+	
+	public void testSocketConnect() {
+		SocketConnect socketConnect = new SocketConnect();
+		socketConnect.connect("127.0.0.1", 80);
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		SocketConnect socketConnect = new SocketConnect();
-		socketConnect.connect("127.0.0.1", 80);
+		System.out.println("Dummy main func");
 	}
 
 }
